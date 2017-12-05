fef2d0d47b4a1b8ff3d4dd4000440b2af3b34cc2
Iakov 'Jake' Kirilenko <Jake.Kirilenko@gmail.com>
2017-12-05 16:58:25 +0300

    MSP430: timeout delay after reset

--- recipes-bsp/firmware/files/msp_reset.sh
+++ recipes-bsp/firmware/files/msp_reset.sh
@@ -7,5 +7,12 @@ usleep 10000
 echo 1 > /sys/class/gpio/gpio93/value
 usleep 10000
 echo 0 > /sys/class/gpio/gpio80/value
+sleep 3
 echo Done
-lsusb
+if lsusb | grep -q '2047:0200'
+then
+    exit 0
+else
+    echo "MSP reset failed!"
+    exit 1
+fi
