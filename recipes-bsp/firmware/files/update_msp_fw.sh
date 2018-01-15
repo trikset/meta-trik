1b3a412a2ce0983e7d5954f66b7a11a5dc32a17d
Iakov 'Jake' Kirilenko <Jake.Kirilenko@gmail.com>
2018-01-15 11:51:04 +0300

    msp-firmware: ver 0D with f5528 support

--- recipes-bsp/firmware/files/update_msp_fw.sh
+++ recipes-bsp/firmware/files/update_msp_fw.sh
@@ -1,5 +1,30 @@
 #!/bin/sh -e
-FWNAME=`/bin/ls /etc/trik/msp430/msp-firmware-*.txt | /usr/bin/head -n 1`
+
+hex2dec() {
+    local res
+    res=`printf "%d" "$1" 2>/dev/null`
+    # do not return correct substring of incorrect string
+    # if previous printf was incorrect, make another attempt
+    if [ $? -ne 0 ] ; then
+       res=`printf "%d" "0x$1" 2>/dev/null`
+    fi
+    echo $res
+}
+
+
+# If MSP does not respond we try to reset it and to re-flash
+# Assume i2c-tools are installed ...
+old=`i2cget -y 2  0x48 0xee w || echo 0xffff`
+
+MCU=10
+case ${old} in
+ 0xff* | 0x10* ) MCU=10 ;;
+ 0x28*) MCU=28 ;;
+ *) echo ERROR in version detection ;;
+esac
+
+FWNAME=`/bin/ls /etc/trik/msp430/msp-firmware-$MCU*.txt | head -n 1`
+
 getNewMspFwVer() {
     local ver
     ver=${FWNAME#*/msp-firmware-}
@@ -7,19 +32,13 @@ getNewMspFwVer() {
     echo $ver
 }
 
-hex2dec() {
-    printf "%d" "$1" 2>/dev/null || printf "%d" "0x$1" 2>/dev/null
-}
+new=`getNewMspFwVer`
 
-# let's assume i2c-tools are installed ....
-NVER=`getNewMspFwVer`
-
-# if MSP does not respond we try to reset it and to re-flash
-OVER=`i2cget -y 2  0x48 0xee || echo 0`
-
-if test `hex2dec $NVER` -ne `hex2dec $OVER` ; then 
-    echo "Updating MSP firmware to version " $NVER
+if [ -z "$new" ] ; then
+  echo Missing new firmware!
+elif test `hex2dec $new` -ne `hex2dec $old` ; then
+    echo "Updating MSP firmware from $old to $new"
     /etc/trik/msp430/msp_reset.sh
-    /usr/sbin/msp-flasher -o $FWNAME
+    /usr/sbin/msp-flasher -o $FWNAME || echo Problem ONE
+    /usr/sbin/msp-flasher -o $FWNAME || echo Problem TWO
 fi
-
