7c80b7478cba7fc3a799200c327378032df46195
Iakov 'Jake' Kirilenko <Jake.Kirilenko@gmail.com>
2018-02-08 13:04:02 +0300

    xsetroot on boot

--- recipes-trik/trik-runtime/files/trikGui.sh
+++ recipes-trik/trik-runtime/files/trikGui.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 . /etc/profile.d/locale.sh
 . /etc/profile.d/qws_display.sh
+which xsetroot >/dev/null &&  xsetroot -solid blue || echo "Missing xsetroot!"
+which xset >/dev/null && ( xset s off ; xset s noblank ; xset -dpms ) || echo "Missing xset!"
 . /etc/profile.d/trik_runtime_path.sh
 . /etc/profile.d/python_path.sh
-which xset && ( xset s off ; xset s noblank ; xset -dpms ) || echo "Missing xset!"
 cd /home/root/trik && exec nice -n -5 ./trikGui 2>&1 1> /dev/null
