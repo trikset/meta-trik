cab3938530e1a0aa1f565871dd292beb468834e1
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2018-12-21 20:38:56 +0300

    Added exec to final function calls

--- recipes-connectivity/trik-uart-ppp/files/init_tty.sh
+++ recipes-connectivity/trik-uart-ppp/files/init_tty.sh
@@ -18,11 +18,11 @@ device="/dev/ttyS1"
 baudrate=115200
 echo_en="echo -en"
 
-login() {
-  getty -L $device $baudrate -n -l /etc/trik/autologin
+exec_login() {
+  exec getty -L $device $baudrate -n -l /etc/trik/autologin
 }
 
-start_pppd() {
+exec_start_pppd() {
   mknod /dev/ppp c 108 0
   exec pppd $device $baudrate 10.0.5.2:10.0.5.1 \
     connect 'chat -v -f /etc/ppp/winclient.chat' \
@@ -36,8 +36,8 @@ $echo_en "Type r to run console\n\r"
 read -t 5 -n 1 ch
 if [ "$ch" == "r" ]; then
   $echo_en "Running console...\n\r"
-  login
+  exec_login
 else 
   $echo_en "Running pppd...\n\r"
-  start_pppd
+  exec_start_pppd
 fi
