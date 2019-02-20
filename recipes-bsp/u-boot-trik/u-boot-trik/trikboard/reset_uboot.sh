f9dc893b8acd941777c6d046a2f0eddfaf247975
Kirill K. Smirnov <kirill.k.smirnov@gmail.com>
2019-02-20 03:46:59 +0300

    trik-u-boot: fix file name
    Signed-off-by: Kirill K. Smirnov <kirill.k.smirnov@gmail.com>

--- recipes-bsp/u-boot-trik/u-boot-trik/trikboard/reset_uboot.sh
+++ recipes-bsp/u-boot-trik/u-boot-trik/trikboard/reset_uboot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
-/usr/sbin/flashcp -v /usr/share/u-boot-trik/u-boot-gzip.ais /dev/mtd0
+/usr/sbin/flashcp -v /usr/share/u-boot-trik/u-boot.ais /dev/mtd0
 /usr/sbin/flash_erase /dev/mtd1 0 0
 
