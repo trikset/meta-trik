fec75206ffb92d0b694248dd3151ad720755301b
Andrei Khodko <andrei.khodko@gmail.com>
2021-10-13 10:01:49 +0300

    Codefactor fix

--- recipes-trik/trik-mems/files/init_mems.sh
+++ recipes-trik/trik-mems/files/init_mems.sh
@@ -3,19 +3,19 @@ set -ueo pipefail
 [ -r /etc/default/trik/mems_options.sh ] && source /etc/default/trik/mems_options.sh || :
 
 set_up_accel() {
-	echo 1 > /sys/bus/iio/devices/iio\:device0/buffer/enable
-	echo 1 > /sys/bus/iio/devices/iio\:device0/scan_elements/in_timestamp_en
-	echo 1 > /sys/bus/iio/devices/iio\:device0/scan_elements/in_accel_x_en
-	echo 1 > /sys/bus/iio/devices/iio\:device0/scan_elements/in_accel_y_en
-	echo 1 > /sys/bus/iio/devices/iio\:device0/scan_elements/in_accel_z_en
+	echo 1 > /sys/bus/iio/devices/iio:device0/buffer/enable
+	echo 1 > /sys/bus/iio/devices/iio:device0/scan_elements/in_timestamp_en
+	echo 1 > /sys/bus/iio/devices/iio:device0/scan_elements/in_accel_x_en
+	echo 1 > /sys/bus/iio/devices/iio:device0/scan_elements/in_accel_y_en
+	echo 1 > /sys/bus/iio/devices/iio:device0/scan_elements/in_accel_z_en
 }
 
 set_up_gyro() {
-	echo 1 > /sys/bus/iio/devices/iio\:device1/buffer/enable
-	echo 1 > /sys/bus/iio/devices/iio\:device1/scan_elements/in_timestamp_en
-	echo 1 > /sys/bus/iio/devices/iio\:device1/scan_elements/in_anglvel_x_en
-	echo 1 > /sys/bus/iio/devices/iio\:device1/scan_elements/in_anglvel_y_en
-	echo 1 > /sys/bus/iio/devices/iio\:device1/scan_elements/in_anglvel_z_en
+	echo 1 > /sys/bus/iio/devices/iio:device1/buffer/enable
+	echo 1 > /sys/bus/iio/devices/iio:device1/scan_elements/in_timestamp_en
+	echo 1 > /sys/bus/iio/devices/iio:device1/scan_elements/in_anglvel_x_en
+	echo 1 > /sys/bus/iio/devices/iio:device1/scan_elements/in_anglvel_y_en
+	echo 1 > /sys/bus/iio/devices/iio:device1/scan_elements/in_anglvel_z_en
 }
 
 main() {
