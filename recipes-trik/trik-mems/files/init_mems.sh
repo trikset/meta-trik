0a3e9d83d10b74a50de0c6103334f78ac3a043c3
Andrei Khodko <andrei.khodko@gmail.com>
2021-10-18 14:19:25 +0300

    Device name as variable

--- recipes-trik/trik-mems/files/init_mems.sh
+++ recipes-trik/trik-mems/files/init_mems.sh
@@ -2,25 +2,17 @@
 set -ueo pipefail
 [ -r /etc/default/trik/mems_options.sh ] && source /etc/default/trik/mems_options.sh || :
 
-set_up_accel() {
-	echo 1 > /sys/bus/iio/devices/iio:device0/buffer/enable
-	echo 1 > /sys/bus/iio/devices/iio:device0/scan_elements/in_timestamp_en
-	echo 1 > /sys/bus/iio/devices/iio:device0/scan_elements/in_accel_x_en
-	echo 1 > /sys/bus/iio/devices/iio:device0/scan_elements/in_accel_y_en
-	echo 1 > /sys/bus/iio/devices/iio:device0/scan_elements/in_accel_z_en
-}
-
-set_up_gyro() {
-	echo 1 > /sys/bus/iio/devices/iio:device1/buffer/enable
-	echo 1 > /sys/bus/iio/devices/iio:device1/scan_elements/in_timestamp_en
-	echo 1 > /sys/bus/iio/devices/iio:device1/scan_elements/in_anglvel_x_en
-	echo 1 > /sys/bus/iio/devices/iio:device1/scan_elements/in_anglvel_y_en
-	echo 1 > /sys/bus/iio/devices/iio:device1/scan_elements/in_anglvel_z_en
+set_up_device() {
+	echo "$1" > /sys/bus/iio/devices/"$2"/buffer/enable
+	echo "$1" > /sys/bus/iio/devices/"$2"/scan_elements/in_timestamp_en
+	echo "$1" > /sys/bus/iio/devices/"$2"/scan_elements/in_accel_x_en
+	echo "$1" > /sys/bus/iio/devices/"$2"/scan_elements/in_accel_y_en
+	echo "$1" > /sys/bus/iio/devices/"$2"/scan_elements/in_accel_z_en
 }
 
 main() {
-	set_up_accel
-	set_up_gyro
+	set_up_device 1 iio:device1
+	set_up_device 1 iio:device0
 }
 
 main
